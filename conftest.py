c090bfb033d49a769718d13512a6ca0c2a27b7e9
79304 <zb_lol@mail.ru>
2022-04-06 09:19:42 +0300

    added base_page.py , conftest.py, requirements.txt

---
+++ conftest.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+@pytest.fixture(scope='function')
+def driver():
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    yield driver
+    driver.quit()
---
+++ pages/base_page.py
@@ -0,0 +1,32 @@
+from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+class BasePage:
+    def __init__(self, driver, url):
+        self.driver = driver
+        self.url = url
+
+    def open(self):
+        self.driver.get(self.url)
+
+    def element_is_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
+
+    def elements_are_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
+
+    def element_is_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
+
+    def elements_are_present(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
+
+    def element_is_not_visible(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.invisibility_of_element_located(locator))
+
+    def element_is_clickable(self, locator, timeout=5):
+        return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
+
+    def go_to_element(self, element):
+        self.driver.execute_script("argument[0].scrollIntoView();", element)
